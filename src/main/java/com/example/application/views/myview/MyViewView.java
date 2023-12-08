67065e0d04bde8b6ab2f5acaa71282b5602b0c53
start.vaadin.com <no-reply@vaadin.com>
2023-12-08 09:04:21 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/myview/MyViewView.java
@@ -0,0 +1,19 @@
+package com.example.application.views.myview;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("My View")
+@Route(value = "my-view")
+@Uses(Icon.class)
+public class MyViewView extends Composite<VerticalLayout> {
+
+    public MyViewView() {
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+    }
+}
